6c6e6efc4fefdd51d85d88b7663d26f844db644d
twarbelow <taija.warbelow@google.com>
2020-08-17 16:32:30 -0700

    grammar change to welcome message

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature "welcome page" do
   it "has a brief description of the application" do
     visit "/"
-    expect(page).to have_content("Welcome to Covid Viewing, your favorite site for finding your next covid watch party movie, and scheduling a time to watch it with all your friends! ")
+    save_and_open_page
+    expect(page).to have_content("Welcome to Covid Viewing! Your favorite site for finding your next covid watch party movie, and scheduling a time to watch it with all your friends!")
   end
 end
