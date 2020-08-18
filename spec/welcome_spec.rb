1141181ef93595322779affc8b3563b773523498
Taija W <taija.warbelow@gmail.com>
2020-08-17 19:11:05 -0700

    Update spec/welcome_spec.rb
    remove save and open page

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature "welcome page" do
   it "has a brief description of the application" do
     visit "/"
-    save_and_open_page
     expect(page).to have_content("Welcome to Covid Viewing! Your favorite site for finding your next covid watch party movie, and scheduling a time to watch it with all your friends!")
   end
 end
