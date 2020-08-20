f0e6544670490388d6235458595266e5205f4d32
Lito White <leland.white@gmail.com>
2020-08-20 13:47:48 -0600

    Updated syntax
    Co-authored-by: Taija Warbelow <10294841+twarbelow@users.noreply.github.com>
    
    Co-authored-by: Lito White <62251085+ljwhite@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,6 @@
 class SessionsController < ApplicationController
 
   def create
-    client_id = ENV["GOOGLE_CLIENT_ID"]
-    client_secret = ENV["GOOGLE_CLIENT_SECRET"]
-    code = params[:code]
     user = User.update_or_create(request.env["omniauth.auth"])
     session[:id] = user.id
     redirect_to root_path
