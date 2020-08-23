e667acb82f1b9ad602077a1190a5872546470c73
twarbelow <taija.warbelow@google.com>
2020-08-23 16:51:22 -0700

    spec for searching movies

---
+++ spec/features/movies/search_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "as an authorized user" do
+  before do
+    login_as_user
+  end
+
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: "google",
+      uid: "12345678910",
+      info: {
+        email: "lito@gmail.com",
+        first_name: "Lito",
+        last_name: "White"
+      },
+      credentials: {
+        token: "abcdefg12345",
+        refresh_token: "12345abcdefg",
+        expires_at: DateTime.now,
+      }
+    })
+  end
+
+  it "I can search the movies and see the first 40 results" do
+      stub_omniauth
+      visit root_path
+      click_link "Sign in with Google"
+
+      visit search_path
+
+      expect(page).to have_content("Search All Movies")
+      fill_in "Search", with: "Star Wars"
+      click_button "Search"
+
+      expect(page).to have_button_or_link("Find Top Rated Movies")
+      expect(page).to have_css(".result", count: 40)
+      expect(page).to have_css(".form")
+  end
+end
