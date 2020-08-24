0c961fc15e3efd3f150fbb397ea9370fa8bf7b90
twarbelow <taija.warbelow@google.com>
2020-08-24 16:04:15 -0700

    final changes before merge

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,8 +2,4 @@ class MoviesController < ApplicationController
   def index
     @results = MovieDB.new.search(params[:query]).map { |movie| Movie.new(movie) } unless params[:query].nil?
   end
-
-  def top_rated
-
-  end
 end
