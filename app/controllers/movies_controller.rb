3b73fc19415d72828dce5392801f240555977f7b
twarbelow <taija.warbelow@google.com>
2020-08-24 15:18:35 -0700

    rubocop fixes, remove empty search action from movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,9 @@
 class MoviesController < ApplicationController
   def index
-    if params[:query] != nil
+    return unless params[:query] = nil
       @results = (MovieDB.new.search(params[:query])).map do |movie|
         Movie.new(movie)
       end
     end
-  end
-
-  def show
-    
   end
 end
--- app/services/movie_db.rb
+++ app/services/movie_db.rb
@@ -1,13 +1,13 @@
 class MovieDB
   def initialize
-    @response_1 = []
-    @response_2 = []
+    @response1 = []
+    @response2 = []
   end
   def search(query)
     search_responses(query)
     # response_1 = conn.get("/3/search/movie?api_key=#{ENV['MOVIEDB_API_KEY']}&sort_by=popularity.desc&page=1&query=#{query}")
     # response_2 = conn.get("/3/search/movie?api_key=#{ENV['MOVIEDB_API_KEY']}&sort_by=popularity.desc&page=2&query=#{query}")
-    first_40_results(@response_1, @response_2)
+    first_40_results(@response1, @response2)
     # page_1 = JSON.parse(response_1.body, symbolize_names: true)
     # page_1 = page_1[:results]
     # page_2 = JSON.parse(response_2.body, symbolize_names: true)
@@ -15,17 +15,17 @@ class MovieDB
     # page_1.concat(page_2)
   end
 
-  def first_40_results(response_1, response_2)
-    page_1 = JSON.parse(response_1.body, symbolize_names: true)
-    page_1 = page_1[:results]
-    page_2 = JSON.parse(response_2.body, symbolize_names: true)
-    page_2 = page_2[:results]
-    page_1.concat(page_2)
+  def first_40_results(response1, response2)
+    page1 = JSON.parse(response1.body, symbolize_names: true)
+    page1 = page1[:results]
+    page2 = JSON.parse(response2.body, symbolize_names: true)
+    page2 = page2[:results]
+    page1.concat(page2)
   end
 
   def search_responses(query)
-    @response_1 = conn.get("/3/search/movie?api_key=#{ENV['MOVIEDB_API_KEY']}&sort_by=popularity.desc&page=1&query=#{query}")
-    @response_2 = conn.get("/3/search/movie?api_key=#{ENV['MOVIEDB_API_KEY']}&sort_by=popularity.desc&page=2&query=#{query}")
+    @response1 = conn.get("/3/search/movie?api_key=#{ENV['MOVIEDB_API_KEY']}&sort_by=popularity.desc&page=1&query=#{query}")
+    @response2 = conn.get("/3/search/movie?api_key=#{ENV['MOVIEDB_API_KEY']}&sort_by=popularity.desc&page=2&query=#{query}")
   end
 
   def top_rated
