5dbffdc9b68e24b49a7171cb8b84aef2a15c9158
twarbelow <taija.warbelow@google.com>
2020-08-24 17:01:50 -0700

    implement VCR in movies/movies details page spec

--- spec/features/movies/movie_details_page_spec.rb
+++ spec/features/movies/movie_details_page_spec.rb
@@ -6,22 +6,17 @@ RSpec.feature 'As a user' do
   end
 
   scenario "Clicking on the title of a movie on the movie results page will send me to the movie show page" do
-    visit movies_discover_path
+    VCR.use_cassette('top_rated_content') do
+      visit movies_discover_path
 
-    top_rated_json_response = File.read('spec/fixtures/top_rated_content.json')
-    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV["API_KEY"]}").
-      to_return(status: 200, body: top_rated_json_response )
+      click_button("Find Top-Rated Movies")
+    end
+    VCR.use_cassette('godfather_content') do
+      click_link "The Godfather"
 
-    click_button("Find Top-Rated Movies")
-
-    godfather_json_response = File.read('spec/fixtures/godfather_content.json')
-    stub_request(:get, "https://api.themoviedb.org/3/movie/238?api_key=#{ENV["API_KEY"]}").
-      to_return(status: 200, body: godfather_json_response)
-    click_link "The Godfather"
-
-    expect(page).to have_content("The Godfather")
-    expect(page).to have_content("An offer you can't refuse")
-
+      expect(page).to have_content("The Godfather")
+      expect(page).to have_content("An offer you can't refuse")
+    end
   end
 
 
