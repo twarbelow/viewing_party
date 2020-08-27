ccab53c1b86b13f1d51f75027818aa11282f9d93
twarbelow <taija.warbelow@google.com>
2020-08-26 21:30:48 -0700

    fix fill_in for the date

--- spec/features/viewing_party/create_spec.rb
+++ spec/features/viewing_party/create_spec.rb
@@ -20,6 +20,9 @@ RSpec.feature 'As a user' do
       expect(page).to have_link("Create Viewing Party")
       click_link "Create Viewing Party"
       expect(page).to have_content("The Godfather")
+      fill_in "Duration", with: "178"
+      fill_in "Date and Time", with: Time.new(2022, 01, 02, 12)
+      click_on "Create View party"
     end
 
 
